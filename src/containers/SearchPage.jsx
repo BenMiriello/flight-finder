d16d447f78fd8b32c982dff3bc8b1a0af2383239
BenMiriello <benmiriello@gmail.com>
2020-02-22 20:50:15 -0500

    recieving first flight_offer data from back end

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -2,11 +2,40 @@ import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 
 export default class SearchPage extends Component {
+
+    state={
+        flightOffers: []
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/v1/flight_offers/204')
+        .then(r => r.json())
+        .then(data => {
+            console.log(data.id)
+            this.setState(prevState => ({
+                flightOffers: [...prevState.flightOffers, data]
+            }))
+        })
+        .then(console.log)
+    }
+
+    showFlightOffers = () => {
+        console.log(this.state.flightOffers[0])
+        return (
+            <p>
+                {/* some text here */}
+                {this.state.flightOffers.length >= 1 ? this.state.flightOffers[0].id : null }
+            </p>
+        )
+    }
+    
     render() {
         return (
             <div>
                 <SearchBar/>
+                {this.showFlightOffers()}
             </div>
         )
     }
 }
+
