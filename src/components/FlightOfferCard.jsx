62541df81a64c14767baf482f0d84a9a9a1de3c4
BenMiriello <benmiriello@gmail.com>
2020-02-23 10:45:47 -0500

    made basic flight offer show card, displaying grand_total, airline icons, # stops

---
+++ src/components/FlightOfferCard.jsx
@@ -0,0 +1,246 @@
+import React, { Component } from 'react'
+import { Table, Card, Item, Image } from 'semantic-ui-react'
+
+export default class FlightOfferCard extends Component {
+
+    toRow = () => {
+        let stopsTo = this.props.flightOffer.itineraries[0].segments.length - 1
+        let carrier = this.props.flightOffer.itineraries[0].segments[0].carrier_code
+        let duration
+        return(
+            <>
+                <Item className="foci foci-carrier-icon">
+                    <Image src={`https://www.gstatic.com/flights/airline_logos/32px/${carrier}.png`}/>
+                </Item>
+                <Item className="foci foci-times" />
+                <Item className="foci foci-legs">
+                    <p>{stopsTo === 0 ? "non" : stopsTo}stop{stopsTo >= 2 ? "s" : ""}</p>
+                </Item>
+                <Item className="foci foci-duration"  /> 
+            </>
+        )
+    }
+
+    fromRow = () => {
+        let stopsFrom = this.props.flightOffer.itineraries[1].segments.length
+        let carrier = this.props.flightOffer.itineraries[1].segments[1].carrier_code
+        return(
+            <>
+            <Item className="foci foci-carrier-icon">
+                <Image src={`https://www.gstatic.com/flights/airline_logos/32px/${carrier}.png`}/>
+            </Item>
+            <Item className="foci foci-times" />
+            <Item className="foci foci-legs">
+                <p>{stopsFrom === 0 ? "non" : stopsFrom}stop{stopsFrom >= 2 ? "s" : ""}</p>
+            </Item>
+            <Item className="foci foci-duration" />
+            </>
+        )
+    }
+
+    render() {
+        console.log('props from flight card: ', this.props);
+        return (
+            <Card className="flight-offer-card" style={{"margin": "auto", "width": "82%"}} >
+                <Table definition>
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell width={13}>
+                                <div className="flight-offer-card-flex-container">
+                                    {this.toRow()}
+                                    {this.fromRow()}
+                                </div>
+                            </Table.Cell>
+                            <Table.Cell>
+                                <p>{this.props.flightOffer.grand_total}</p>
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+            </Card>
+        )
+    }
+}
+
+// props from flight card:  
+// {flightOffer: {…}}
+//     flightOffer:
+//         one_way: false
+//         currency: "EURO"
+//         grand_total: 329
+//         fare_type: "PUBLISHED"
+//         validating_airline_codes: "AY"
+
+//         itineraries: Array(2)
+//             0:
+//                 id: 407
+//                 duration: "PT6H50M"
+//                 segments: Array(1)
+//                     0:
+//                         id: 526
+//                         itinerary_id: 407
+//                         departure_iata: "JFK"
+//                         departure_city_code: "NYC"
+//                         departure_country_code: "US"
+//                         departure_terminal: "7"
+//                         departure_time: "2020-10-01T07:55:00.000Z"
+//                         arrival_iata: "LHR"
+//                         arrival_city_code: "LON"
+//                         arrival_country_code: "GB"
+//                         arrival_terminal: "5"
+//                         arrival_time: "2020-10-01T19:45:00.000Z"
+//                         carrier_code: "AY"
+//                         carrier: "FINNAIR"
+//                         flight_number: "5478"
+//                         aircraft_code: "744"
+//                         aircraft: "BOEING 747-400"
+//                         operating_carrier_code: "BA"
+//                         operating_carrier: null
+//                         duration: "PT6H50M"
+//                         xid: 8
+//                         number_of_stops: 0
+//                         blacklisted_in_eu: false
+//                         created_at: "2020-02-22T22:55:27.810Z"
+//                         updated_at: "2020-02-22T22:55:27.810Z"
+//                         __proto__: Object
+//                     length: 1
+//                     __proto__: Array(0)
+//                 __proto__: Object
+//             1:
+//                 id: 408
+//                 duration: "PT11H55M"
+//                 segments: Array(2)
+//                     0:
+//                         id: 527
+//                         itinerary_id: 408
+//                         departure_iata: "LHR"
+//                         departure_city_code: "LON"
+//                         departure_country_code: "GB"
+//                         departure_terminal: "5"
+//                         departure_time: "2020-10-10T07:20:00.000Z"
+//                         arrival_iata: "MAD"
+//                         arrival_city_code: "MAD"
+//                         arrival_country_code: "ES"
+//                         arrival_terminal: "4S"
+//                         arrival_time: "2020-10-10T10:50:00.000Z"
+//                         carrier_code: "IB"
+//                         carrier: "IBERIA"
+//                         flight_number: "7460"
+//                         aircraft_code: "320"
+//                         aircraft: "AIRBUS INDUSTRIE A320-100/200"
+//                         operating_carrier_code: "BA"
+//                         operating_carrier: null
+//                         duration: "PT2H30M"
+//                         xid: 21
+//                         number_of_stops: 0
+//                         blacklisted_in_eu: false
+//                         created_at: "2020-02-22T22:55:27.832Z"
+//                         updated_at: "2020-02-22T22:55:27.832Z"
+//                         __proto__: Object
+//                     1:
+//                         id: 528
+//                         itinerary_id: 408
+//                         departure_iata: "MAD"
+//                         departure_city_code: "MAD"
+//                         departure_country_code: "ES"
+//                         departure_terminal: "4S"
+//                         departure_time: "2020-10-10T12:20:00.000Z"
+//                         arrival_iata: "JFK"
+//                         arrival_city_code: "NYC"
+//                         arrival_country_code: "US"
+//                         arrival_terminal: "7"
+//                         arrival_time: "2020-10-10T14:15:00.000Z"
+//                         carrier_code: "IB"
+//                         carrier: "IBERIA"
+//                         flight_number: "6251"
+//                         aircraft_code: "359"
+//                         aircraft: "AIRBUS INDUSTRIE A350-900"
+//                         operating_carrier_code: "IB"
+//                         operating_carrier: null
+//                         duration: "PT7H55M"
+//                         xid: 22
+//                         number_of_stops: 0
+//                         blacklisted_in_eu: false
+//                         created_at: "2020-02-22T22:55:27.844Z"
+//                         updated_at: "2020-02-22T22:55:27.844Z"
+//                         __proto__: Object
+//                     length: 2
+//                     __proto__: Array(0)
+//                 __proto__: Object
+//             length: 2
+//             __proto__: Array(0)
+
+//         travelers: Array(1)
+//             0:
+//                 id: 204
+//                 traveler_type: "ADULT"
+//                 currency_code: "EUR"
+//                 currency: "EURO"
+//                 total: 329
+//                 traveler_segments: Array(3)
+//                     0:
+//                         id: 1
+//                         traveler_id: 204
+//                         segment_id: 526
+//                         segment_xid: 8
+//                         cabin: "ECONOMY"
+//                         fare_basis: "OLN8C7B5"
+//                         branded_fare: "NOBAG"
+//                         rbd_class: "O"
+//                         included_checked_bags_quantity: 0
+//                         created_at: "2020-02-22T22:55:27.888Z"
+//                         updated_at: "2020-02-22T22:55:27.888Z"
+//                         __proto__: Object
+//                     1:
+//                         id: 2
+//                         traveler_id: 204
+//                         segment_id: 527
+//                         segment_xid: 21
+//                         cabin: "ECONOMY"
+//                         fare_basis: "OLN8C7B5"
+//                         branded_fare: "NOBAG"
+//                         rbd_class: "O"
+//                         included_checked_bags_quantity: 0
+//                         created_at: "2020-02-22T22:55:27.894Z"
+//                         updated_at: "2020-02-22T22:55:27.894Z"
+//                         __proto__: Object
+//                     2:
+//                         id: 3
+//                         traveler_id: 204
+//                         segment_id: 528
+//                         segment_xid: 22
+//                         cabin: "ECONOMY"
+//                         fare_basis: "OLN8C7B5"
+//                         branded_fare: "NOBAG"
+//                         rbd_class: "O"
+//                         included_checked_bags_quantity: 0
+//                         created_at: "2020-02-22T22:55:27.900Z"
+//                         updated_at: "2020-02-22T22:55:27.900Z"
+//                         __proto__: Object
+//                     length: 3
+//                     __proto__: Array(0)
+//                 __proto__: Object
+//             length: 1
+//             __proto__: Array(0)
+//         __proto__: Object
+//     __proto__: Object
+
+
+
+{/* <Item className="flight-offer-card-item" />
+    {/* <Item.Image size='small' src='https://images.unsplash.com/photo-1503780099440-e6ab046a1d71?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&h=500&q=100' /> */}
+
+{/* <Item className="foci foci-50" />
+<Item className="foci foci-50" />
+<Item className="foci" /> */}
+
+// .foci-carrier-icon {
+//   width: 44px
+// .foci-times {
+//   width: 25%;
+// .foci-legs {
+//   width: 12%
+// .foci-duration {
+//   width: 12%
+// .carrier-icon
+
--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
+import FlightOfferCard from '../components/FlightOfferCard'
+import { Container } from 'semantic-ui-react'
 
 export default class SearchPage extends Component {
 
@@ -11,7 +13,6 @@ export default class SearchPage extends Component {
         fetch('http://localhost:3000/api/v1/flight_offers/204')
         .then(r => r.json())
         .then(data => {
-            console.log(data.id)
             this.setState(prevState => ({
                 flightOffers: [...prevState.flightOffers, data]
             }))
@@ -22,18 +23,17 @@ export default class SearchPage extends Component {
     showFlightOffers = () => {
         console.log(this.state.flightOffers[0])
         return (
-            <p>
-                {/* some text here */}
-                {this.state.flightOffers.length >= 1 ? this.state.flightOffers[0].id : null }
-            </p>
+            this.state.flightOffers.map(foobj => <FlightOfferCard flightOffer={foobj} />)
         )
     }
-    
+
     render() {
         return (
             <div>
                 <SearchBar/>
-                {this.showFlightOffers()}
+                <Container className="flight-offer-cards-container">
+                    {this.showFlightOffers()}
+                </Container>
             </div>
         )
     }
